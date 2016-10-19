5dada04c82e66dc8c35817b79dd318aa53d17948
Jan-Ivar Bruaroey <wingboy@gmail.com>
2016-10-19 14:38:27 -0400

    Initial version.

---
+++ blacksilence.js
@@ -0,0 +1,13 @@
+let silence = () => {
+  let ctx = new AudioContext(), oscillator = ctx.createOscillator();
+  let dst = oscillator.connect(ctx.createMediaStreamDestination());
+  oscillator.start();
+  return Object.assign(dst.stream.getAudioTracks()[0], {enabled: false});
+}
+
+let black = ({width = 640, height = 480} = {}) => {
+  let canvas = Object.assign(document.createElement("canvas"), {width, height});
+  canvas.getContext('2d').fillRect(0, 0, width, height);
+  let stream = canvas.captureStream();
+  return {track: Object.assign(stream.getVideoTracks()[0], {enabled: false}), stream};
+}
